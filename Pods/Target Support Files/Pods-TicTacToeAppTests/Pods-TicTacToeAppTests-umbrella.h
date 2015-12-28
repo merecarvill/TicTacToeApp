cd1a709469f0a36f1a00f371116ff1e7c99247e2
Spencer <scarvill91@gmail.com>
2015-12-28 15:58:02 -0600

    Set up Quick and Nimble

---
+++ Pods/Target Support Files/Pods-TicTacToeAppTests/Pods-TicTacToeAppTests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_TicTacToeAppTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_TicTacToeAppTestsVersionString[];
+
--- TicTacToeAppTests/TicTacToeAppTests.swift
+++ TicTacToeAppTests/TicTacToeAppTests.swift
@@ -1,36 +1,17 @@
-//
-//  TicTacToeAppTests.swift
-//  TicTacToeAppTests
-//
-//  Created by Spencer Carvill on 12/28/15.
-//  Copyright © 2015 scarvill. All rights reserved.
-//
+import Quick
+import Nimble
 
-import XCTest
-@testable import TicTacToeApp
+class FooSpec: QuickSpec {
+  override func spec() {
 
-class TicTacToeAppTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock {
-            // Put the code you want to measure the time of here.
+    describe("something") {
+
+      context("when something else") {
+
+        it("is true") {
+          expect(true)
         }
+      }
     }
-    
+  }
 }
